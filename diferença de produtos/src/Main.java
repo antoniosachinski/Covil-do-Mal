f7beb53aae6fefc7d63dd96a0c3d9fc8b2a365bd
Antonio Sachinski <as.sachinski@gmail.com>
2025-03-24 01:14:11 -0300

    docs: adiciona comentários

--- diferença de produtos/src/Main.java
+++ diferença de produtos/src/Main.java
@@ -1,3 +1,5 @@
+// fazer um programa para ler quatro valores inteiros A, B, C e D. A seguir, calcule e mostre a diferença do produto de A e B pelo produto de C e D segundo a fórmula: DIFERENCA = (A * B - C * D)
+
 import java.util.Scanner;
 
 public class Main {
