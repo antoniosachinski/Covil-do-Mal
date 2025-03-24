8c13931287e21a97d73138bfc3d504a9934e81ee
Antonio Sachinski <as.sachinski@gmail.com>
2025-03-24 01:23:56 -0300

    feat(estudo): métodos getters e setters

---
+++ métodos especiais/src/pacote/Classe.java
@@ -0,0 +1,23 @@
+package pacote;
+
+public class Classe {
+	
+	String A;
+	  int B;
+
+	  public String getA() {
+	    return this.A;
+	  }
+
+	  public void setA(String x) {
+	    this.A = x;
+	  }
+
+	  public int getB() {
+	    return this.B;
+	  }
+
+	  public void setB(int y) {
+	    this.B = y;
+	  }
+}
---
+++ métodos especiais/src/pacote/Main.java
@@ -0,0 +1,14 @@
+package pacote;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Classe objeto1 = new Classe();
+	    objeto1.setA("pão");
+	    objeto1.setB(20);
+	    System.out.println(objeto1.getA());
+	    System.out.println(objeto1.getB());
+
+	}
+
+}
